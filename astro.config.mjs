81335c10b644083499df5ea4cf2866caf41a8ba7
Daniel Bengl <daniel.bengl@renuo.ch>
2024-07-11 16:22:26 +0200

    Add base path configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   layout: 'layouts/BaseLayout.astro',
-  integrations: [mdx()]
+  integrations: [mdx()],
+  site: "https://cuddlybunion341.github.io/astro-blog",
 });
