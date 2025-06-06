675d8e520d09a8e9609c612127a5231621a7d47f
Daniel Bengl <daniel.bengl@renuo.ch>
2025-06-06 12:30:56 +0200

    Rid tailwind, styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig, sharpImageService } from "astro/config";
 import markdownConfig from "./markdown.config";
 import mdx from "@astrojs/mdx";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   layout: "layouts/BaseLayout.astro",
@@ -13,7 +11,6 @@ export default defineConfig({
       ...markdownConfig,
       extendPlugins: false,
     }),
-    tailwind({ applyBaseStyles: true, configFilePath: "./tailwind.config.js" }),
   ],
   site: "https://cuddlybunion341.github.io",
   image: {
