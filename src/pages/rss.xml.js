d98d3a5d35adb5a79e85cf353cf91f6567a217c7
Daniel Bengl <53896675+CuddlyBunion341@users.noreply.github.com>
2025-07-27 08:59:30 +0200

    Attempt fix rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,33 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-
 export async function GET(context) {
   const blog = await getCollection("blog");
+
+  const items = await Promise.all(
+    blog.map(async (post) => {
+      const { html } = await post.render();
+
+      return {
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/blog/${post.slug}/`,
+        content: html,
+        categories: post.data.tags || [],
+        author: "CuddlyBunion341",
+        guid: post.slug,
+        ...(post.data.heroImage && {
+          enclosure: {
+            url: post.data.heroImage,
+            type: "image/jpeg",
+            length: 0,
+          },
+        }),
+        customData: `
+          <tags>${(post.data.tags || []).map((tag) => `<tag>${tag}</tag>`).join("")}</tags>
+          <license>MIT</license>
+        `,
+      };
+    })
+  );
 
   return rss({
     title: "CuddlyBunion341 Blog",
@@ -13,26 +38,6 @@ export async function GET(context) {
     lastBuildDate: new Date(),
     ttl: 60,
     customData: `<generator>Astro v4</generator>`,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/blog/${post.slug}/`,
-      content: post.body,
-      categories: post.data.tags || [],
-      author: "CuddlyBunion341",
-      guid: post.slug,
-      ...(post.data.heroImage && {
-        enclosure: {
-          url: post.data.heroImage,
-          type: "image/jpeg",
-          length: 0,
-        },
-      }),
-      customData: `
-        <tags>${(post.data.tags || []).map((tag) => `<tag>${tag}</tag>`).join("")}</tags>
-        <license>MIT</license>
-      `,
-    })),
+    items,
   });
 }
